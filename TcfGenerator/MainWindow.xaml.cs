f6b613e5f0a3e960b16a72df1de1a72a7dc1319b
WANG <jian-hui_wang@keysight.com>
2018-04-13 14:26:16 +0800

    Update the Mapping graph as nested DataGrid, the outer DataGrid shows the Excel -> TapStep Mapping, and the inner DataGrid shows the Excel Value -> Tap Value Mapping.

--- TcfGenerator/MainWindow.xaml.cs
+++ TcfGenerator/MainWindow.xaml.cs
@@ -19,13 +19,14 @@ namespace TcfGenerator
 {
     public partial class MainWindow : Window
     {
-
-        public ObservableCollection<ExcelMapping> MappingRules { get; set; }
+        public ObservableCollection<SettingMapping> SettingMappings { get; set; }
         public ObservableCollection<Tuple<string,string>> TestStepList { get; set; }
         public ObservableCollection<Tuple<string,string,string>> PropList { get; set; }
 
+        private ObservableCollection<ValueMapping> ValueMappings { get; set; }
         private XmlNode Node_TestSteps;
         private int teststep_idx;
+        private int rule_idx;
 
         public MainWindow()
         {
@@ -33,10 +34,12 @@ namespace TcfGenerator
 
             TestStepList = new ObservableCollection<Tuple<string,string>>();
             PropList = new ObservableCollection<Tuple<string,string,string>>();
-            MappingRules = new ObservableCollection<ExcelMapping>();
+            SettingMappings = new ObservableCollection<SettingMapping>();
+            ValueMappings = new ObservableCollection<ValueMapping>();
+            rule_idx = 0;
 
             InitializeTestStepList(@"c:\temp\test.xml");
-            tapMapping.DataContext = MappingRules;
+            tapMapping.DataContext = SettingMappings;
             testStep.DataContext = this;
             Property.DataContext = this;
         }
@@ -76,19 +79,23 @@ namespace TcfGenerator
 
         private void Add_One(object sender, RoutedEventArgs e)
         {
-            ExcelMapping m = new ExcelMapping();
+            SettingMapping m = new SettingMapping();
             int teststep_idx = testStep.SelectedIndex;
             int property_idx = Property.SelectedIndex;
 
             if (teststep_idx != -1 && property_idx != -1)
             {
+                m.Serial = ++rule_idx;
                 m.ExcelColumn = excelColumn.Text;
                 m.TestStep = TestStepList[teststep_idx].Item1;
                 m.TestStep_DispName = TestStepList[teststep_idx].Item2 ;
                 m.Property = PropList[property_idx].Item1;
                 m.Property_DispName = PropList[property_idx].Item2;
                 m.PropertyType = PropList[property_idx].Item3;
-                MappingRules.Add(m);
+                m.ValMapping = ValueMappings;
+                SettingMappings.Add(m);
+
+                tapMapping.Items.Refresh();
             }
         }
 
@@ -96,17 +103,64 @@ namespace TcfGenerator
         {
             int index = tapMapping.SelectedIndex;
             if (index == -1) return;
-            MappingRules.RemoveAt(index);
+            SettingMappings.RemoveAt(index);
+            rule_idx--;
+            for (int i = index; i<SettingMappings.Count; i++)
+            {
+                SettingMappings[i].Serial = i + 1;
+            }
+            tapMapping.Items.Refresh();
         }
+
+        private void PropertyChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (teststep_idx == -1) return;
+            int property_idx = (sender as ComboBox).SelectedIndex;
+            if (property_idx == -1) return;
+
+            var teststep = Node_TestSteps.ChildNodes[teststep_idx];
+
+            ValueMappings.Clear();
+
+            foreach(XmlNode p in teststep.ChildNodes)
+            {
+                if (PropList[property_idx].Item1 == p.Attributes["Name"].Value)
+                {
+                    var typeAttr = p.Attributes["Type"];
+                    var availValuesAttr = p.Attributes["AvailValues"];
+
+                    if ((typeAttr != null && typeAttr.Value == "System.String" && availValuesAttr != null && availValuesAttr.Value == "True")
+                        || typeAttr != null && typeAttr.Value.StartsWith("Enumeration"))
+                    {
+                        var values = p.ChildNodes;
+                        foreach (XmlNode v in values)
+                        {
+                            ValueMappings.Add(new TcfGenerator.ValueMapping() { TapValue = v.InnerText, ExcelValue = "" });
+                        }
+                    }
+                }
+            }
+
+        }
+
     }
 
-    public class ExcelMapping
+    public class SettingMapping
     {
+        public int Serial { get; set; }
         public string ExcelColumn { get; set; }
         public string TestStep { get; set; }
         public string TestStep_DispName { get; set; }
         public string Property { get; set; }
         public string Property_DispName{ get; set; }
         public string PropertyType { get; set; }
+        public ObservableCollection<ValueMapping> ValMapping { get; set; }
     }
+
+    public class ValueMapping
+    {
+        public string ExcelValue { get; set; }
+        public string TapValue { get; set; }
+    }
+
 }
